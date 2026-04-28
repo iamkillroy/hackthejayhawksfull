b4aa618d3132c7d508a110011e2f47fd936a8ce9
iamkillroy <lucasfrias@mailfence.com>
2026-04-28 12:58:30 -0500

    Added basic enemy follow path code

--- src/entity/enemy.py
+++ src/entity/enemy.py
@@ -1,11 +1,12 @@
 
-
+from pygame import clock
 class Enemy(Entity):
 	def __init__(self, x, y, screen, health):
 		super.__init__(self, x, y, screen)
 		self.health = health
 		self.cooldown = 2
-		self.coordNodes = [(0,0,), (100,100), (200,200), (300,300)]
+		self.coordNodes = [(0,0), (100,100), (200,200), (300,300)]
+		self.currentCoordNodes = 0
 	def update(self):
 		#this is our moving algo:
 		# you can think of pretty much the shortest relative path as the distance between
@@ -16,3 +17,15 @@ class Enemy(Entity):
 		#  /
 		# E
 		# (1,1)
+		# so now we pretty miuch have the position of x,y
+		# and we're able to basically move the position from
+		dt = clock.tick(30) / 1000 #get deltatime
+		#check for x, y 
+		if round(self.x) > round(self.currentCoordNodes[0][0]) 
+		or round(self.x) < round(self.currentCoordNodes)[0][0]:
+			self.x += 1 * dt #x
+		elif round(self.y) > round(self.currentCoordNodes[0][1]) 
+		or round(self.y) < round(self.currentCoordNodes)[0][1]:
+			self.y += 1 * dt #y
+		else: #if all is false, we're at the right point and += 1 
+			self.currentCoordNodes += 1 if self.currentCoordNodes < self.coordNodes else 0
