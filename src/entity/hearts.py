4162b68f1ea81c5efe6d8298767b5f56294105da
iamkillroy <lucasfrias@mailfence.com>
2026-05-01 13:31:13 -0500

    Added cartoid graphics

--- src/entity/hearts.py
+++ src/entity/hearts.py
@@ -1,18 +1,26 @@
 from .entity import Entity
 import pygame
 
+
 class Heart(Entity):
-	def __init__(self, x, y, screen):
-		self.entityName = "hearts"
-		super().__init__(x, y, screen)
-		self.health = 8
-		self.startX = x
-		self.startY = y
-		#this is a carbon copy of the previous gamemap rescale, look for comments there
-		self.sprites = list()
-		scaledHeart = pygame.transform.scale_by(pygame.image.load(self.get_sprite_addr(0)), 4)
-		self.sprites.append(scaledHeart)
-	def display(self):
-	    pass
-	def update(self, dt):
-	    super().update(dt)
+    def __init__(self, x, y, screen):
+        self.entityName = "hearts"
+        super().__init__(x, y, screen)
+        self.health = 5
+        self.startX = x
+        self.startY = y
+        # this is a carbon copy of the previous gamemap rescale, look for comments there
+        self.sprites = list()
+        scaledHeart = pygame.transform.scale_by(
+            pygame.image.load(self.get_sprite_addr(0)), 3
+        )
+        self.sprites.append(scaledHeart)
+
+    def display(self):
+        for i in range(self.health):
+            self.screen.blit(
+                self.sprites[self.spriteState], (self.x + (i * 60), self.y)
+            )
+
+    def update(self, dt):
+        super().update(dt)
